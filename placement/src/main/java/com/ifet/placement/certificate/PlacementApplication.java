12b42951c42647fe04992739ac08619e2e087803
alwindonald <alwindonald4004@gmail.com>
2025-04-22 22:29:26 +0530

    backend commit

--- placement/src/main/java/com/ifet/placement/certificate/PlacementApplication.java
+++ placement/src/main/java/com/ifet/placement/certificate/PlacementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlacementApplication {
 
 	public static void main(String[] args) {
+		// run in application context beam
 		SpringApplication.run(PlacementApplication.class, args);
 	}
 }
